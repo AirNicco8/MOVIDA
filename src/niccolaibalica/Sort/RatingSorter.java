178bac5fade909492622ab01123db5e862f4d4dd
DatNigazz <lornic98@gmail.com>
2020-08-18 17:55:09 +0200

    commented comparators

--- src/niccolaibalica/Sort/RatingSorter.java
+++ src/niccolaibalica/Sort/RatingSorter.java
@@ -9,6 +9,9 @@ public class RatingSorter implements Comparator<Movie> {
       @Override
       public int compare(Movie o1, Movie o2) {
         return o1.getVotes() - o2.getVotes();
+        /* -return >0 ---> o1 ha più voti
+           -return <0 ---> o1 ha meno voti
+        */
       }
 
 }
--- src/niccolaibalica/Sort/YearSorter.java
+++ src/niccolaibalica/Sort/YearSorter.java
@@ -10,5 +10,10 @@ public class YearSorter implements Comparator<Movie> {
       public int compare(Movie o1, Movie o2) {
         return o1.getYear().compareTo(o2.getYear());
       }
+      /*
+      - return 0 ---> anno di o1 = anno di o2;
+      - return <0 ---> anno di o1 è minore di anno di o2 (più vecchio)
+      - return >0 ---> anno di o1 è maggiore di o2 (più recente)
+      */
 
 }
